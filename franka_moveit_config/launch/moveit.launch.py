7ae7e11401de5080fa27aea7c48436559369e025
hazemy <youssef@iwt-bodensee.de>
2025-02-18 14:57:18 +0100

    added environment description files

--- franka_moveit_config/launch/moveit.launch.py
+++ franka_moveit_config/launch/moveit.launch.py
@@ -58,6 +58,8 @@ def generate_launch_description():
     # planning_context
     franka_xacro_file = os.path.join(get_package_share_directory('franka_description'), 'robots',
                                      'panda_arm.urdf.xacro')
+    # franka_xacro_file = os.path.join(get_package_share_directory('franka_description'), 'robots',
+    #                                  'panda_arm_with_env.urdf.xacro')
     robot_description_config = Command(
         [FindExecutable(name='xacro'), ' ', franka_xacro_file, ' hand:=true',
          ' robot_ip:=', robot_ip, ' use_fake_hardware:=', use_fake_hardware,
